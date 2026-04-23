2c4d0013af7fa9aeda4a4e7eed99250ce55b1382
s_takemasa <s_takemasa@LAPTOP-D6LI18JV>
2026-04-23 10:38:26 +0900

    プロジェクト新規作成

---
+++ src/main/java/kouka/Question4.java
@@ -0,0 +1,24 @@
+package kouka;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns={"/kouka/Question4"})
+public class Question4 extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        
+        String name = request.getParameter("name");
+        
+        System.out.println("お名前：" + name);
+                request.setAttribute("name", name);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("Question4output.jsp");
+        dispatcher.forward(request, response);
+    }
+}
---
+++ src/main/webapp/kouka/Question4output.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>Question4 出力画面</title>
+</head>
+<body>
+    <h1>Question4</h1>
+    <p>入力されたお名前は・・・</p>
+    <p>${name} ですね！</p>
+</body>
+</html>
